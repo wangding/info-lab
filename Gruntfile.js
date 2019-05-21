1d6e3419c50813e9d5d223775935a257511dafaa
wangding <408542507@qq.com>
2019-05-21 13:42:10 +0800

    chore: 增加了 eslint 自动化任务

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,17 +47,23 @@ module.exports = function (grunt) {
         ]
       }
     },
+    htmlhint: {
+      options: {
+        htmlhintrc: '.htmlhintrc'
+      },
+      src: ['*.html', 'labs/*.html']
+    },
     csslint: {
       options: {
         csslintrc: '.csslintrc'
       },
       src: ['labs/css/*.css']
     },
-    htmlhint: {
+    eslint: {
       options: {
-        htmlhintrc: '.htmlhintrc'
+        configFile: '.eslintrc.json'
       },
-      src: ['*.html', 'labs/*.html']
+      target: ['./labs/js/*.js']
     }
   });
 
@@ -65,11 +71,11 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
-  //grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-htmlhint');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-eslint');
 
-  grunt.registerTask('lint', ['htmlhint', 'csslint']);
+  grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
   grunt.registerTask('build', ['htmlmin', 'cssmin', 'uglify', 'imagemin']);
 };
